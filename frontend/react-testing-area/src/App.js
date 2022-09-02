d6ce38c66bac5fb262c1aad66513380c511b96d9
JTrevena <jacobtrevena@gmail.com>
2022-09-02 17:26:00 +0100

    basic lookup features working for words in dictionary

--- frontend/react-testing-area/src/App.js
+++ frontend/react-testing-area/src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
+import {useState, useEffect} from "react";
 const fetchDefinitions = require("./networking/fetchDefinitions")
-
 const BACKEND_PATH = "http://localhost:8000"
 
-async function handleSubmitWord(e) {
-  e.preventDefault();
-  const word = e.target[0].value;
-  const response = await fetchDefinitions(BACKEND_PATH, word);
-  console.log(response);
-}
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("Reference");
+  const [results, setResults] = useState({definitions: []});
+  
+  async function handleSubmitWord(e) {
+    e.preventDefault();
+    const word = e.target[0].value;
+    const results = await fetchDefinitions(BACKEND_PATH, word);
+    setCurrentWord(word);
+    setResults(results);
+  }
+
+  function displayResults() {
+    return results.definitions.map((result) => {
+      return (
+        <div>
+          <div className="definition-parent">
+            <p className="definition-child-cla">{result.classification}</p>
+            <p className="definition-child-def">{result.definition}</p>
+          </div>
+        </div>
+      )
+    })
+  }
+  
   return (
     <div className="App">
       <h1>Look up a word!</h1>
@@ -18,6 +36,8 @@ function App() {
         <input id="word-input" type="text"/>
         <button id="word-submit" type="submit">Submit!</button>
       </form>
+      <h1>{currentWord}</h1>
+      <div>{displayResults()}</div>
     </div>
   );
 }
