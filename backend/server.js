f67a354e88a01e0e0eeb0fc13aadb54243ccddc2
JTrevena <jacobtrevena@gmail.com>
2022-09-02 16:55:40 +0100

    implemented CORS

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const handleGetDefinition = require("./handlerFunctions/handleGetDefinition")
+
 PORT = 8000;
+CORS = {
+    "origin": "http://localhost:3000",
+    "content-type": "application/json"
+}
 
-
-app.get("/example", (req,res) => {
-    const {text} = req.query;
-    const msg = "This is an example response.\nYou entered the text: " + (text ? text : "");
-    const instructions = "Use the query parameter 'text' to send some text";
-
-    res.status(200).json({instructions, msg})
-})
+app.use(cors(CORS))
 app.get("/definition/:word", (req, res) => handleGetDefinition(req, res))
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
